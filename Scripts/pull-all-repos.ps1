bd9b4aa94b56d35e1531bf684d7a812d6e5053d7
Rob Goodridge <rob.goodridge@lansa.com.au>
2022-09-13 07:49:08 +0000

    163728 tidy up push-all-repos

--- Scripts/pull-all-repos.ps1
+++ Scripts/pull-all-repos.ps1
@@ -1,7 +1,12 @@
 [string[]] $SecondaryRoots = @("C:\Program Files (x86)\AZURESQL", "C:\Program Files (x86)\LANSA", "C:\Program Files (x86)\SQLANYWHERE")
 
+Push-Location
+
 foreach ($Root in $SecondaryRoots) {
     Set-Location "$Root\lansa\VersionControl"
     Get-Location | Write-Host
     git pull | Write-Host
+    Write-Host
 }
+
+Pop-Location
