9708b21e249ea5556edff3df9aebe4ae410f109f
Rob Goodridge <rob.goodridge@lansa.com.au>
2022-09-22 06:45:34 +0000

    163728 Neaten script

--- Scripts/pull-all-repos.ps1
+++ Scripts/pull-all-repos.ps1
@@ -1,8 +1,8 @@
-[string[]] $SecondaryRoots = @("C:\Program Files (x86)\AZURESQL", "C:\Program Files (x86)\LANSA", "C:\Program Files (x86)\SQLANYWHERE", "C:\Program Files (x86)\ORACLE")
+[string[]] $Roots = @("C:\Program Files (x86)\AZURESQL", "C:\Program Files (x86)\LANSA", "C:\Program Files (x86)\SQLANYWHERE", "C:\Program Files (x86)\ORACLE")
 
 Push-Location
 
-foreach ($Root in $SecondaryRoots) {
+foreach ($Root in $Roots) {
     Set-Location "$Root\lansa\VersionControl"
     Get-Location | Write-Host
     git pull | Write-Host
