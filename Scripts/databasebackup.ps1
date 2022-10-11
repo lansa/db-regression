99dd297a5deda12707022259e0a24a5c1f3bf450
Tarun <t.somavarapu@isi-info.com>
2022-10-11 14:59:50 +0530

    changed the set aws creds

--- Scripts/databasebackup.ps1
+++ Scripts/databasebackup.ps1
@@ -1,12 +1,11 @@
-Set-AWSCredentials -AccessKey AKIA3JDVQNWWHFSBKU55 -SecretKey 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 -StoreAs myAWScredentials
-#Set-AWSCredentials myAWScredentials
+Set-AWSCredentials myAWScredentials
 
 $server = 'dbregressiontest.database.windows.net'
 $backupPath = 'C:\Program Files (x86)\AZURESQL'
 $s3bucket = 'lansa-us-east-1/db-regression-test/backups/150050/'
 $region = 'us-east-1'
 
-$databases = Invoke-Sqlcmd -ServerInstance $server -Username "lansa" -Password "Pcxuser@122robg" -Query "SELECT [name]
+$databases = Invoke-Sqlcmd -ServerInstance $server -Username $user -Password $password -Query "SELECT [name]
 FROM master.dbo.sysdatabases where [name]='test'"
 
 foreach ($database in $databases)
