81af33bd0c4dd02ab9d316072ad69e5d79bf1d2e
Tarun <t.somavarapu@isi-info.com>
2022-11-01 12:00:03 +0530

    updated the clone lansa version

--- Scripts/azure_stack_provision.ps1
+++ Scripts/azure_stack_provision.ps1
@@ -2,6 +2,7 @@ param (
 	[parameter(Mandatory=$true)]
 	[string] $lansa_version,
 
+	[parameter(Mandatory=$false)]
 	[string] $clone_lansa_version, ## lansa version that would be used to create clone stack.
 
 #############As AWS and Azure not working from same script expecting the below values from another script"aws_secret_manager" or pipeline
@@ -50,7 +51,7 @@ if($azure_tags.count -eq 1){
         Write-Host "Not found the Database $lansa_version, checking for source server."
         if ($sourceserver.count -eq 1) {
             Write-Host "Source server found $sourceserver, Restoring..."
-		    New-AzSqlDatabaseCopy -ResourceGroupName dbregressiontest -ServerName $sourceserver -DatabaseName $db -CopyResourceGroupName dbregressiontest -CopyServerName $sql_server -CopyDatabaseName $lansa_version | Out-Default | Write-Host
+		    New-AzSqlDatabaseCopy -ResourceGroupName dbregressiontest -ServerName $sourceserver -DatabaseName $clone_lansa_version -CopyResourceGroupName dbregressiontest -CopyServerName $sql_server -CopyDatabaseName $lansa_version | Out-Default | Write-Host
         }else {
             throw "Found more than 1 Azure sql server with the Lansa Version tag = $clone_lansa_version"
         }
@@ -62,7 +63,7 @@ else {
     Write-Host "Created the SQL server, Now Restoring the Database using $clone_lansa_version to $lansa_version..."
     if($sourceserver.count -eq 1){
         Write-Host "Source server found $sourceserver, Restoring..."
-		New-AzSqlDatabaseCopy -ResourceGroupName dbregressiontest -ServerName $sourceserver -DatabaseName $db -CopyResourceGroupName dbregressiontest -CopyServerName $sql_server -CopyDatabaseName $lansa_version | Out-Default | Write-Host
+		New-AzSqlDatabaseCopy -ResourceGroupName dbregressiontest -ServerName $sourceserver -DatabaseName $clone_lansa_version -CopyResourceGroupName dbregressiontest -CopyServerName $sql_server -CopyDatabaseName $lansa_version | Out-Default | Write-Host
     }
     else {
         throw "Found more than 1 Azure sql server with the Lansa Version tag = $clone_lansa_version"
