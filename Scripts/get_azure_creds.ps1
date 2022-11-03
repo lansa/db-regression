45b19fbb684f4f55dab460f69e61af5f803cca39
Tarun <t.somavarapu@isi-info.com>
2022-11-03 16:19:18 +0530

    updated the select object

--- Scripts/get_azure_creds.ps1
+++ Scripts/get_azure_creds.ps1
@@ -1,6 +1,6 @@
-$sql_username = Get-SECSecretValue -SecretId "password/DBRegressionTest/SQLAZURE" -Select SecretString | ConvertFrom-Json | Select-Object -ExpandProperty UID
+$sql_username = Get-SECSecretValue -SecretId "password/DBRegressionTest/SQLAZURE" -Select SecretString | ConvertFrom-Json | Select -ExpandProperty UID
 
-$sql_password = Get-SECSecretValue -SecretId "password/DBRegressionTest/SQLAZURE" -Select SecretString | ConvertFrom-Json | Select-Object -ExpandProperty PWD #| ConvertTo-SecureString -AsPlainText -Force
+$sql_password = Get-SECSecretValue -SecretId "password/DBRegressionTest/SQLAZURE" -Select SecretString | ConvertFrom-Json | Select -ExpandProperty PWD #| ConvertTo-SecureString -AsPlainText -Force
 
 
 "##vso[task.setvariable variable=username]$sql_username"
