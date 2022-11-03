b747c5bfa6e9afb08ea394e772ef8df7944210d5
Tarun <t.somavarapu@isi-info.com>
2022-11-03 13:48:26 +0530

    converted the password as securestring

--- Scripts/import_script_test.ps1
+++ Scripts/import_script_test.ps1
@@ -7,17 +7,17 @@ param (
 	[string]$sql_username,
 
 	[parameter(Mandatory=$true)]
-	[string]$sql_password
+	[SecureString]$sql_password
 )
 
-$sql_password = ConvertTo-SecureString $azure_sql_password -AsPlainText -Force
+#$sql_password = ConvertTo-SecureString $azure_sql_password -AsPlainText -Force
 
 $storage_key = (Get-AzStorageAccountKey -ResourceGroupName "dbregressiontest" -StorageAccountName "stagingdpuseast").Value[0]
 $storage_url = "https://stagingdpuseast.blob.core.windows.net/azuresqlbackup"
 $storage_uri = "$storage_url/$lansa_version/$lansa_version.bacpac"
 $sql_server = "db-regression-$lansa_version"
 
-$importRequest = New-AzSqlDatabaseImport -ResourceGroupName "dbregressiontest" -ServerName $sql_server -DatabaseName $clone_lansa_version -StorageKeyType "StorageAccessKey" -StorageKey $storage_key -StorageUri $storage_uri -AdministratorLogin $sql_username -AdministratorLoginPassword $azure_sql_password -Edition GeneralPurpose -ServiceObjectiveName GP_S_Gen5_8 -DatabaseMaxSizeBytes 1099511627776
+$importRequest = New-AzSqlDatabaseImport -ResourceGroupName "dbregressiontest" -ServerName $sql_server -DatabaseName $clone_lansa_version -StorageKeyType "StorageAccessKey" -StorageKey $storage_key -StorageUri $storage_uri -AdministratorLogin $sql_username -AdministratorLoginPassword $sql_password -Edition GeneralPurpose -ServiceObjectiveName GP_S_Gen5_8 -DatabaseMaxSizeBytes 1099511627776
 		
 #cheacking status
 $importStatus = Get-AzSqlDatabaseImportExportStatus -OperationStatusLink $importRequest.OperationStatusLink
