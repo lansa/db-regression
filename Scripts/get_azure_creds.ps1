667380d0ee16704d0632198c3d8e83f260e17ec7
Tarun <t.somavarapu@isi-info.com>
2022-11-03 13:57:13 +0530

    setting variable issecret to true

--- Scripts/get_azure_creds.ps1
+++ Scripts/get_azure_creds.ps1
@@ -3,9 +3,5 @@ $sql_username = Get-SECSecretValue -SecretId "password/DBRegressionTest/SQLAZURE
 $sql_password = Get-SECSecretValue -SecretId "password/DBRegressionTest/SQLAZURE" -Select SecretString | ConvertFrom-Json | Select-Object -ExpandProperty PWD | ConvertTo-SecureString -AsPlainText -Force
 
 
-
-#$azusername = Get-SECSecretValue -SecretId azuresql-username -Select SecretString | ConvertFrom-Json | Select -ExpandProperty username
-#$azpassword = Get-SECSecretValue -SecretId azuresql-username -Select SecretString | ConvertFrom-Json | Select -ExpandProperty password | ConvertTo-SecureString -AsPlainText -Force
-
 "##vso[task.setvariable variable=username]$sql_username"
-"##vso[task.setvariable variable=password]$sql_password"
+"##vso[task.setvariable variable=password;issecret=true]$sql_password"
