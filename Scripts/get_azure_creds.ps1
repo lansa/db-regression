9e3701d97e26fc78b67c6735c40708fe4467fdd0
Tarun <t.somavarapu@isi-info.com>
2022-11-03 14:38:41 +0530

    converted the string to securestring

--- Scripts/get_azure_creds.ps1
+++ Scripts/get_azure_creds.ps1
@@ -1,7 +1,7 @@
 $sql_username = Get-SECSecretValue -SecretId "password/DBRegressionTest/SQLAZURE" -Select SecretString | ConvertFrom-Json | Select-Object -ExpandProperty UID
 
-$sql_password = Get-SECSecretValue -SecretId "password/DBRegressionTest/SQLAZURE" -Select SecretString | ConvertFrom-Json | Select-Object -ExpandProperty PWD | ConvertTo-SecureString -AsPlainText -Force
+$sql_password = Get-SECSecretValue -SecretId "password/DBRegressionTest/SQLAZURE" -Select SecretString | ConvertFrom-Json | Select-Object -ExpandProperty PWD #| ConvertTo-SecureString -AsPlainText -Force
 
 
 "##vso[task.setvariable variable=username]$sql_username"
-"##vso[task.setvariable variable=password;issecret=true]$sql_password"
+"##vso[task.setvariable variable=password]$sql_password"
