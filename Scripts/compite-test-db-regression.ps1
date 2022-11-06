25d1861c0f1b25bb518f1852c4ccdc5d410dcf2c
nkhl-accolite <88071306+nkhl-accolite@users.noreply.github.com>
2022-11-06 12:24:11 +0530

    Create compite-test-db-regression.ps1
    Instead of running this in the pipeline inline, this file will be used to trigger `db-test.ps1`.

---
+++ Scripts/compite-test-db-regression.ps1
@@ -0,0 +1,42 @@
+# This script uses the PowerShellSend-SSMCommand API to run db-test.ps1
+
+param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    $LansaVersion,
+
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Test,
+    
+    [Parameter(Mandatory = $false)]
+    [string]
+    $Compile,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $OutputS3BucketName,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $OutputS3KeyPrefix
+)
+
+$runPSCommandID = (Send-SSMCommand `
+        -DocumentName "AWS-RunPowerShellScript" `
+        -Comment "Running db-test.ps1 on DB-Regression-VM-$LansaVersion" `
+        -Parameter @{'commands'=@(' & "C:\Program Files (x86)\Lansa\LANSA\VersionControl\Scripts\db-test.ps1" -Compile $Compile -Test $Test')} `
+        -Target @(@{Key="tag:aws:cloudformation:stack-name"; Values = "DB-Regression-VM-$LansaVersion"}, @{Key="tag:LansaVersion"; Values = $LansaVersion}) `
+        -OutputS3BucketName $OutputS3BucketName `
+        -OutputS3KeyPrefix $OutputS3KeyPrefix).CommandId
+
+
+while ($true) {
+    if ((Get-SSMCommand -CommandId $runPSCommandID).Status -eq "Success") {
+            Write-Host "Execution complete. The logs can be found here:  $(OutputS3BucketName)/$(OutputS3KeyPrefix)/$runPSCommandID" # When the Status of the Get-SSMCommand is "Success", the while loop terminates
+            break
+        } else { # While the status isn't "Success"...
+            Start-Sleep 20 # Checking every 20 seconds, if the status changes to "Success", 
+            Write-Host "Please wait, db-test.ps1 execution is in progress." # and if not, writing to host that message.
+        }
+}
