fb70f4791ec628acfb1cb8e8c684630137d094ff
nkhl-accolite <88071306+nkhl-accolite@users.noreply.github.com>
2022-11-06 15:53:10 +0530

    Update compile-test-db-regression.ps1

--- Scripts/compile-test-db-regression.ps1
+++ Scripts/compile-test-db-regression.ps1
@@ -25,7 +25,7 @@ param (
 $runPSCommandID = (Send-SSMCommand `
         -DocumentName "AWS-RunPowerShellScript" `
         -Comment "Running db-test.ps1 on DB-Regression-VM-$LansaVersion" `
-        -Parameter @{'commands'=@(' & "C:\Program Files (x86)\Lansa\LANSA\VersionControl\Scripts\db-test.ps1" -Compile $Compile -Test $Test')} `
+        -Parameter @{'commands'=@(' & "C:\Program Files (x86)\Lansa\LANSA\VersionControl\Scripts\db-test.ps1 -Compile $Compile -Test $Test" ')} `
         -Target @(@{Key="tag:aws:cloudformation:stack-name"; Values = "DB-Regression-VM-$LansaVersion"}, @{Key="tag:LansaVersion"; Values = $LansaVersion}) `
         -OutputS3BucketName $OutputS3BucketName `
         -OutputS3KeyPrefix $OutputS3KeyPrefix).CommandId
@@ -33,7 +33,7 @@ $runPSCommandID = (Send-SSMCommand `
 
 while ($true) {
     if ((Get-SSMCommand -CommandId $runPSCommandID).Status -eq "Success") {
-            Write-Host "Execution complete. The logs can be found here:  $OutputS3BucketName/$OutputS3KeyPrefix/$runPSCommandID" # When the Status of the Get-SSMCommand is "Success", the while loop terminates
+            Write-Host "Execution complete. The logs can be found here: $OutputS3BucketName/$OutputS3KeyPrefix/$runPSCommandID" # When the Status of the Get-SSMCommand is "Success", the while loop terminates
             break
         } else { # While the status isn't "Success"...
             Start-Sleep 20 # Checking every 20 seconds, if the status changes to "Success", 
