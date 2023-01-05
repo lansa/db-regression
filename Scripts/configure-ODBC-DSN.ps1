c8ac9251ace9b7bb99368613d558e0077e578f4d
Rob Goodridge <rob.goodridge@lansa.com.au>
2023-01-05 07:05:06 +0000

    163728 Change location of Oracle configuration file to c:\oracle\tnsnames.ora

--- Scripts/configure-ODBC-DSN.ps1
+++ Scripts/configure-ODBC-DSN.ps1
@@ -8,12 +8,12 @@ $DSNNames = (Get-OdbcDsn).Name
 
 # AZURESQL
 
-if($DSNNames -contains "AZURESQL" -and (Get-OdbcDsn -Name "AZURESQL").Platform -eq "32-bit"){
+if ($DSNNames -contains "AZURESQL" -and (Get-OdbcDsn -Name "AZURESQL").Platform -eq "32-bit"){
 
-    $sqlserver = "db-regression-" + $lansaVersion + ".database.windows.net" 
+    $sqlserver = "db-regression-" + $lansaVersion + ".database.windows.net"
 
     Write-Host("Updating Server name as $sqlserver and database name as $lansaVersion for AZURESQL")
-    
+
     Set-OdbcDsn -Name "AZURESQL" -DsnType "System" -Platform "32-bit" -SetPropertyValue @("Server=$sqlserver", "Database=$lansaVersion")
 
 }
@@ -21,9 +21,8 @@ if($DSNNames -contains "AZURESQL" -and (Get-OdbcDsn -Name "AZURESQL").Platform -
 
 # Oracle
 
-if($DSNNames -contains "ora19cdb" -and (Get-OdbcDsn -Name "ora19cdb").Platform -eq "32-bit"){
-
-    $TNSNamesPath = "C:\oracle\instantclient_21_6\network\admin\tnsnames.ora"
+$TNSNamesPath = "C:\oracle\tnsnames.ora"
+if (Test-Path $TNSNamesPath){
     $Content = Get-Content $TNSNamesPath
     $oraServer = "ora" + $lansaVersion + ".cnyed5gpqwey.us-east-1.rds.amazonaws.com"
 
@@ -36,7 +35,7 @@ if($DSNNames -contains "ora19cdb" -and (Get-OdbcDsn -Name "ora19cdb").Platform -
 
             $newString = "            (ADDRESS=(PROTOCOL=TCP)(HOST=ora" + $lansaVersion + ".cnyed5gpqwey.us-east-1.rds.amazonaws.com)(PORT=1521))"
             $Content = $Content.replace($string,$newString)
-            
+
         }
     }
     $Content | Set-Content -Path $TNSNamesPath
@@ -45,12 +44,12 @@ if($DSNNames -contains "ora19cdb" -and (Get-OdbcDsn -Name "ora19cdb").Platform -
 
 # MYSQL
 
-if($DSNNames -contains "MYSQL" -and (Get-OdbcDsn -Name "MYSQL").Platform -eq "32-bit"){
+if ($DSNNames -contains "MYSQL" -and (Get-OdbcDsn -Name "MYSQL").Platform -eq "32-bit"){
 
     $mysqlServer = "mysql" + $lansaVersion + ".cnyed5gpqwey.us-east-1.rds.amazonaws.com"
 
     Write-Host("Updating Server name as $mysqlServer and database name as $lansaVersion for MYSQL")
-    
+
     $temp = Get-OdbcDsn -Name "MYSQL"
     Remove-OdbcDsn -Name "MYSQL" -DsnType "System" -Platform "32-bit"
     Add-OdbcDsn -Name "MYSQL" -DriverName $temp.DriverName -Platform $temp.Platform -DsnType $temp.DsnType -SetPropertyValue @("Server=$mysqlServer", "PORT=$temp.Attribute.PORT", "Database=$lansaVersion")
