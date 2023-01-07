d1a16b9fcec423c58de508c9c7675e67c042174b
Rob Goodridge <rob.goodridge@lansa.com.au>
2023-01-07 16:23:12 +1100

    163728 Fix exception occurring when stopping a resource when a resource is already stopped

--- Scripts/aws_stack_stop.ps1
+++ Scripts/aws_stack_stop.ps1
@@ -14,48 +14,58 @@ $MYSQL_DB_INSTANCE_IDENTIFIER = "mysql" + $lansa_version
 
 
 Write-Host "Stopping the VM"
-Stop-EC2Instance -InstanceId $INSTANCE_ID -Force
-$RetryCount = 30
-while (( (((Get-EC2InstanceStatus -IncludeAllInstance $true -InstanceId $INSTANCE_ID).InstanceState).Name).Value -ne "stopped" )  -and ($RetryCount -gt 0) )
-{
-    Write-Host "Waiting for stopping the VM"
-    Start-Sleep -Seconds 20
-    $RetryCount -= 1
+try {
+   Stop-EC2Instance -InstanceId $INSTANCE_ID -Force
+   $RetryCount = 30
+   while (( (((Get-EC2InstanceStatus -IncludeAllInstance $true -InstanceId $INSTANCE_ID).InstanceState).Name).Value -ne "stopped" )  -and ($RetryCount -gt 0) )
+   {
+      Write-Host "Waiting for the VM to stop"
+      Start-Sleep -Seconds 20
+      $RetryCount -= 1
+   }
+   if ($RetryCount -le 0) {
+      Write-Host "##[error] Timeout: 10 minutes expired in waiting for VM with instance id $INSTANCE_ID to be stopped"
+   } else {
+      Write-Host "VM is in stopped state"
+   }
+} catch {
+   Write-Host "VM is already in stopped state or can't be found"
 }
-if ($RetryCount -le 0) {
-    Write-Host "##[error] Timeout: 10 minutes expired in waiting for VM with instance id $INSTANCE_ID to be stopped"
-} else {
-    Write-Host "VM is in stopped state"
-}
-
 
 Write-Host "Stopping the ORACLE RDS"
-Stop-RDSDBInstance -DBInstanceIdentifier $ORACLE_DB_INSTANCE_IDENTIFIER -Force
-$RetryCount = 60
-while (((Get-RDSDBInstance -Filter @{Name="db-instance-id"; Values=$ORACLE_DB_INSTANCE_IDENTIFIER}).DBInstanceStatus -ne "stopped" )  -and ($RetryCount -gt 0) ){
-    Write-Host "Waiting for stopping the Oracle RDS"
-    Start-Sleep -Seconds 20
-    $RetryCount -= 1
-}
+try {
+   Stop-RDSDBInstance -DBInstanceIdentifier $ORACLE_DB_INSTANCE_IDENTIFIER -Force
+   $RetryCount = 60
+   while (((Get-RDSDBInstance -Filter @{Name="db-instance-id"; Values=$ORACLE_DB_INSTANCE_IDENTIFIER}).DBInstanceStatus -ne "stopped" )  -and ($RetryCount -gt 0) ){
+      Write-Host "Waiting for the Oracle RDS to stop..."
+      Start-Sleep -Seconds 20
+      $RetryCount -= 1
+   }
 
-if ($RetryCount -le 0){
-    Write-Host "##[error] Timeout: 20 minutes expired in waiting for ORACLE RDS $ORACLE_DB_INSTANCE_IDENTIFIER to be stopped"
-} else {
-    Write-Host "Oracle RDS is in stopped state"
+   if ($RetryCount -le 0){
+      Write-Host "##[error] Timeout: 20 minutes expired in waiting for ORACLE RDS $ORACLE_DB_INSTANCE_IDENTIFIER to be stopped"
+   } else {
+      Write-Host "Oracle RDS is in stopped state"
+   }
+} catch {
+   Write-Host "Oracle RDS is already in stopped state or can't be found"
 }
-
 
 Write-Host "Stopping the MYSQL RDS"
-Stop-RDSDBInstance -DBInstanceIdentifier $MYSQL_DB_INSTANCE_IDENTIFIER -Force
-$RetryCount = 60
-while (((Get-RDSDBInstance -Filter @{Name="db-instance-id"; Values=$MYSQL_DB_INSTANCE_IDENTIFIER}).DBInstanceStatus -ne "stopped" )  -and ($RetryCount -gt 0) )
-{
-    Write-Host "Waiting for stopping the MYSQL RDS"
-    Start-Sleep -Seconds 20
-    $RetryCount -= 1
-}
-if ($RetryCount -le 0) {
-    Write-Host "##[error] Timeout: 20 minutes expired in waiting for MYSQL RDS $MYSQL_DB_INSTANCE_IDENTIFIER to be stopped"
-} else {
-    Write-Host "MYSQL RDS is in stopped state"
+try {
+   Stop-RDSDBInstance -DBInstanceIdentifier $MYSQL_DB_INSTANCE_IDENTIFIER -Force
+   $RetryCount = 60
+   while (((Get-RDSDBInstance -Filter @{Name="db-instance-id"; Values=$MYSQL_DB_INSTANCE_IDENTIFIER}).DBInstanceStatus -ne "stopped" )  -and ($RetryCount -gt 0) )
+   {
+      Write-Host "Waiting for the MYSQL RDS to stop..."
+      Start-Sleep -Seconds 20
+      $RetryCount -= 1
+   }
+   if ($RetryCount -le 0) {
+      Write-Host "##[error] Timeout: 20 minutes expired in waiting for MYSQL RDS $MYSQL_DB_INSTANCE_IDENTIFIER to be stopped"
+   } else {
+      Write-Host "MYSQL RDS is in stopped state"
+   }
+} catch {
+   Write-Host "MYSQL RDS is already in stopped state or can't be found"
 }
