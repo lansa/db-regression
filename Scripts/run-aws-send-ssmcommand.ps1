0aa7c953c7a2736bcf4fe126ff14f15815c4c3d2
Rob Goodridge <rob.goodridge@lansa.com.au>
2023-12-03 22:38:57 +0000

    Remove invalid default parameter which caused pull-all-repos.ps1 to fail

--- Scripts/run-aws-send-ssmcommand.ps1
+++ Scripts/run-aws-send-ssmcommand.ps1
@@ -9,7 +9,7 @@ param (
 
     [Parameter(Mandatory = $false)]
     [string]
-    $scriptParameters = 'Administrator', # e.g. "-Compile 0 -Test 1" Note that Boolean parameters must be passed as 1 or 0, not $true or $false. The SSM Command cannot pass booleans through to the script
+    $scriptParameters, # e.g. "-Compile 0 -Test 1" Note that Boolean parameters must be passed as 1 or 0, not $true or $false. The SSM Command cannot pass booleans through to the script
 
     [Parameter(Mandatory = $true)]
     [string]
