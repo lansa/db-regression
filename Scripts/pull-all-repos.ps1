4b92e3ef83c8e54f01d7faa7a807c6e5d536321b
Rob Goodridge <rob.goodridge@lansa.com.au>
2023-12-03 23:21:06 +0000

    another fix for pull-all-repos returning exit 1 in pipeline

--- Scripts/pull-all-repos.ps1
+++ Scripts/pull-all-repos.ps1
@@ -1,12 +1,23 @@
 [string[]] $Roots = @("C:\Program Files (x86)\AZURESQL", "C:\Program Files (x86)\LANSA", "C:\Program Files (x86)\SQLANYWHERE", "C:\Program Files (x86)\ORACLE")
 
 Push-Location
+Write-Host "Pulling all repos..."
 
-foreach ($Root in $Roots) {
-    Set-Location "$Root\lansa\VersionControl"
-    Get-Location | Write-Host
-    git pull | Write-Host
-    Write-Host
+try {
+    foreach ($Root in $Roots) {
+        Set-Location "$Root\lansa\VersionControl"
+        Get-Location | Write-Host
+        git pull | Write-Host
+        Write-Host
+    }
+} catch {
+    if ( $LASTEXITCODE -ne 0) {
+        cmd /c exit $LASTEXITCODE
+    } else {
+        cmd /c exit 99
+    }
+    throw "pull-all-repos error"
+} finally {
+    Pop-Location
 }
-
-Pop-Location
+cmd /c exit 0
