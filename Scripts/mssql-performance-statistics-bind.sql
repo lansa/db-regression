4eda6f1cfcf4b97755a5d3b03b71315742d922a9
Periyannan S <Periyannan.s@celestialsys.com>
2024-01-22 10:00:08 +0000

    Issue #49 - Performance Test - Bind Variables

---
+++ Scripts/mssql-performance-statistics-bind.sql
@@ -0,0 +1,28 @@
+-- Execute query to see the performance statistics with bind query.
+-- This is required each time a new VM is created because the HOSTNAME changes.
+-- Note: Variables cannot be used in DDL statments.
+-- Instead, a string needs to be constructed containing the DDL and into which the variable may be concatenated.
+-- Then the string is executed as an SQL script.
+-- To aid debugging the script to be executed is first printed
+USE [LANSA]
+DECLARE @rowCount INT = 1;
+DECLARE @l_description varchar(1000);
+DECLARE @l_start DATETIME2 = GETDATE();
+DECLARE @sstr nvarchar(500);
+
+WHILE @rowCount <= 1000
+BEGIN
+    
+    DECLARE l_rc CURSOR FOR
+		SELECT [F157033K1] FROM [VWBPLIBF].[VTLPerformanceStats] WHERE [F157033K2] = @rowCount
+    OPEN l_rc;	
+    FETCH NEXT FROM l_rc INTO @l_description;
+    CLOSE l_rc;
+    DEALLOCATE l_rc;
+    set @rowCount = @rowCount + 1;
+END
+
+PRINT CONVERT(VARCHAR, DATEDIFF(MILLISECOND, @l_start, GETDATE())) + ' MilliSeconds...';
+
+
+ 
