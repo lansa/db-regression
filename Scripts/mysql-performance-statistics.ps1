eae36e69cd9bd216881a30cb21ad3e4375d0d8b4
Periyannan S <Periyannan.s@celestialsys.com>
2024-01-26 16:14:07 +0000

    mysql host name updated from DNS config

--- Scripts/mysql-performance-statistics.ps1
+++ Scripts/mysql-performance-statistics.ps1
@@ -1,6 +1,19 @@
 $sql_username = Get-SECSecretValue -SecretId "password/DBRegressionTest/MYSQL" -Select SecretString | ConvertFrom-Json | Select -ExpandProperty UID
 $sql_password = Get-SECSecretValue -SecretId "password/DBRegressionTest/MYSQL" -Select SecretString | ConvertFrom-Json | Select -ExpandProperty PWD #| ConvertTo-SecureString -AsPlainText -Force
 
+$DSNNames = (Get-OdbcDsn).Name
+$lansaVersion = "150060"
+ # MYSQL
+
+    if ($DSNNames -contains "MYSQL" -and (Get-OdbcDsn -Name "MYSQL").Platform -eq "32-bit"){
+
+        $mysqlServer = "mysql" + $lansaVersion + ".cnyed5gpqwey.us-east-1.rds.amazonaws.com"
+}
+
+    if ($DSNNames -contains "MYSQL" -and (Get-OdbcDsn -Name "MYSQL").Platform -eq "64-bit"){
+
+        $mysqlServer = "mysql" + $lansaVersion + ".cnyed5gpqwey.us-east-1.rds.amazonaws.com"
+}
 
 #remove if configuration file is exists
 $filecsv = "my.ini.lansa"
@@ -13,7 +26,7 @@ New-Item $filecsv -ItemType File -Value ("[client]" + [Environment]::NewLine) | 
 Add-Content $filecsv ("database=LANSA")
 Add-Content $filecsv ("user=" + $sql_username)
 Add-Content $filecsv ("password=" + $sql_password)
-Add-Content $filecsv ("host=mysql150060.cnyed5gpqwey.us-east-1.rds.amazonaws.com")
+Add-Content $filecsv ("host="+ $mysqlServer)
 
 #execute the performance statistics for both bind and literal
 get-content mysql-performance-statistics-literal.sql | & "C:\Program Files\mysql\MySQL Workbench 8.0 CE\mysql.exe" --defaults-file=my.ini.lansa --skip-column-names
