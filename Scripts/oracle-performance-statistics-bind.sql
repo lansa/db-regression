064d4c03822611f147bdda48f18f60a528d1831f
Periyannan S <Periyannan.s@celestialsys.com>
2024-01-27 08:13:50 +0000

    Oracle SQL script and PS scripts added

---
+++ Scripts/oracle-performance-statistics-bind.sql
@@ -0,0 +1,29 @@
+-- Execute query to see the performance statistics using bind variables.
+
+SET FEEDBACK OFF;
+SET SERVEROUTPUT ON SIZE UNLIMITED;
+SET LINESIZE 300;
+SET TRIMOUT ON;
+SET TRIMSPOOL ON;
+
+DECLARE
+        TYPE rc IS REF CURSOR;
+        l_rc rc;
+        l_description char(100);
+        l_start timestamp := systimestamp;        
+    BEGIN        
+        FOR i IN 1 .. 1000 LOOP            
+            OPEN l_rc FOR
+                'SELECT F157033K1 ' ||
+                'FROM VWBPLIBF.VTLI0049 ' ||
+                'WHERE F157033K2 = :x' USING i;
+            FETCH l_rc INTO l_description;
+            CLOSE l_rc;            
+        END LOOP;
+        dbms_output.put_line('ORACLE, Bind, ' ||
+            to_number(to_char( to_timestamp( trunc( sysdate ) ) + ( systimestamp- l_start ), 'ff3' )) || ', milliseconds'
+        );
+    END;
+/
+
+EXIT;
