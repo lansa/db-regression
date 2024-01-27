602d0adf1521ce0a37a95127b19e306d90278afd
Periyannan S <Periyannan.s@celestialsys.com>
2024-01-27 05:30:14 +0000

    Removed intermediary $sqlserver and $database variables in Azure PS script

--- Scripts/sqlazure-performance-statistics.ps1
+++ Scripts/sqlazure-performance-statistics.ps1
@@ -2,8 +2,6 @@ $sql_username = Get-SECSecretValue -SecretId "password/DBRegressionTest/MSSQLS" 
 $sql_password = Get-SECSecretValue -SecretId "password/DBRegressionTest/MSSQLS" -Select SecretString | ConvertFrom-Json | Select -ExpandProperty PWD #| ConvertTo-SecureString -AsPlainText -Force
 
 $ODBC = Get-OdbcDsn -Name "AZURESQL" -Platform "32-bit" | Select-Object -Expandproperty Attribute
-$sqlserver = $ODBC.server
-$database = $ODBC.database 
 
-SQLCMD.exe -S $sqlserver -d $database -U $sql_username -P $sql_password -i sqlazure-performance-statistics-literal.sql
-SQLCMD.exe -S $sqlserver -d $database -U $sql_username -P $sql_password -i sqlazure-performance-statistics-bind.sql
+SQLCMD.exe -S $ODBC.server -d $ODBC.database -U $sql_username -P $sql_password -i sqlazure-performance-statistics-literal.sql
+SQLCMD.exe -S $ODBC.server -d $ODBC.database -U $sql_username -P $sql_password -i sqlazure-performance-statistics-bind.sql
