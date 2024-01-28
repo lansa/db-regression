7dd7815595186bbba9f9b0cf7bfc27220df109c9
Periyannan S <Periyannan.s@celestialsys.com>
2024-01-28 05:42:16 +0000

    oracle script file renamed in ps1

--- Scripts/odbcoracle-performance-statistics.ps1
+++ Scripts/odbcoracle-performance-statistics.ps1
@@ -1,5 +1,5 @@
 $sql_username = Get-SECSecretValue -SecretId "password/DBRegressionTest/MYSQL" -Select SecretString | ConvertFrom-Json | Select -ExpandProperty UID
 $sql_password = Get-SECSecretValue -SecretId "password/DBRegressionTest/MYSQL" -Select SecretString | ConvertFrom-Json | Select -ExpandProperty PWD #| ConvertTo-SecureString -AsPlainText -Force
 
-sqlplus -s -NOLOGINTIME $sql_username/$sql_password@ora19cdb @oracle-performance-statistics-literal.sql
-sqlplus -s -NOLOGINTIME $sql_username/$sql_password@ora19cdb @oracle-performance-statistics-bind.sql
+sqlplus -s -NOLOGINTIME $sql_username/$sql_password@ora19cdb @odbcoracle-performance-statistics-literal.sql
+sqlplus -s -NOLOGINTIME $sql_username/$sql_password@ora19cdb @odbcoracle-performance-statistics-bind.sql
