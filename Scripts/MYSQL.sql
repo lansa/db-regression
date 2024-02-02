d3e9acdb3d36aef9771ed5b63c968e8b2f061a57
Netravati1205 <netravati.b@celestialsys.com>
2024-02-02 13:19:45 +0000

    Issue-36 | SQL DD Create scripts

---
+++ Scripts/MYSQL.sql
@@ -0,0 +1,23 @@
+USE `LANSA`;
+
+CREATE TABLE `VWBPLIBF_VTLI0036D`(
+	`V_ID` int NOT NULL,
+	`V_NAME` nvarchar(50) NULL,
+	`V_SURNAME` char(10) NULL,
+	`V_LASTNAME` nchar(10) NULL,
+	`V_DATE` datetime(3) NULL,
+	`V_SALARY` Double NULL,
+	`V_ACTIVE` Tinyint NULL,
+	`V_DESC` Longtext NULL,
+	`V_TAG` Char(36) NULL,
+	`V_TIME` time(6) NULL,
+	`V_RAWDATA` binary(50) NULL,
+	`V_DATA` varchar(50) NULL,
+	`V_CODE` numeric(18, 0) NULL,
+	`V_XML` Longtext NULL,
+ CONSTRAINT `PK_VTLI0036D` PRIMARY KEY 
+(
+	`V_ID` ASC
+) 
+) ;
+
