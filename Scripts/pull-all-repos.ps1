b2851c16b6e0eb9dc3c9db252dfd56072403baec
Rob Goodridge <rob.goodridge@lansa.com.au>
2024-03-08 01:26:36 +0000

    Support testing a branch

--- Scripts/pull-all-repos.ps1
+++ Scripts/pull-all-repos.ps1
@@ -1,15 +1,28 @@
+param (
+    [Parameter(Mandatory = $false)]
+    [string]
+    $LansaVersion
+)
+
 [string[]] $Roots = @("C:\Program Files (x86)\AZURESQL", "C:\Program Files (x86)\LANSA", "C:\Program Files (x86)\MYSQL", "C:\Program Files (x86)\SQLANYWHERE", "C:\Program Files (x86)\ORACLE")
 
 Push-Location
 Write-Host "Pulling all repos..."
 
 try {
+    if ([string]::IsNullOrEmpty($LansaVersion) ) {
+        $Branch = 'debug/paas'
+    } else {
+        $Branch = "L4W$($LansaVersion)"
+    }
+
     foreach ($Root in $Roots) {
         Set-Location "$Root\lansa\VersionControl"
         Get-Location | Write-Host
         git reset --hard HEAD | Write-Host
         git clean -f | Write-Host
-        git checkout debug/paas | Write-Host
+        git fetch --all | Write-Host
+        git checkout $Branch | Write-Host
         git pull | Write-Host
         Write-Host
     }
