8e268c65c09c0ab771124a42f739b9eb6a300826
Rob Goodridge <rob.goodridge@lansa.com.au>
2024-04-19 04:08:13 +0000

    Fix up UserList copy and set the branch correctly - change to directory first!

--- Scripts/pull-all-repos.ps1
+++ Scripts/pull-all-repos.ps1
@@ -5,7 +5,7 @@ param (
 )
 
 [string[]] $Roots = @("C:\Program Files (x86)\AZURESQL", "C:\Program Files (x86)\LANSA", "C:\Program Files (x86)\MYSQL", "C:\Program Files (x86)\SQLANYWHERE", "C:\Program Files (x86)\ORACLE")
-#[string[]] $Roots = @("C:\Program Files (x86)\AZURESQL")
+#[string[]] $Roots = @("C:\Program Files (x86)\ORACLE")
 
 Push-Location
 Write-Host "Pulling all repos..."
@@ -21,15 +21,16 @@ try {
 
     foreach ($Root in $Roots) {
         Write-Host "Copy User Lists from MSSQLS IDE WRITE location to Compiler's READ location! "
-        robocopy "C:\Program Files (x86)\LANSA\LANSA\LANSA\UserLists\WBP" "$Root\lansa\UserLists\WBP" *.txt /w:2
+        robocopy "C:\Program Files (x86)\LANSA\LANSA\LANSA\UserLists\WBP" "$Root\lansa\UserLists\WBP" *.txt /w:2 /xo
         
+        Set-Location "$Root\lansa\VersionControl"
+        Get-Location | Write-Host
+
         git show-ref --verify --quiet refs/heads/$branch
         if ( $LASTEXITCODE -ne 0) {
             Write-Host "$Branch branch does not exist. Switching to debug/paas"
             $Branch = 'debug/paas'
         }
-        Set-Location "$Root\lansa\VersionControl"
-        Get-Location | Write-Host
         Write-Host "Clean out current git state so that following operations can succeed."
         git reset --hard HEAD | Write-Host
         if ( $LASTEXITCODE -ne 0) { throw }
