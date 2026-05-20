9aaac52215836ed85b87badc67a59f7f09c50d6b
Michael Mendy <montana@linux.com>
2026-05-20 10:23:34 -0700

    notation added back

--- Scripts/mysql-performance-metrics.ps1
+++ Scripts/mysql-performance-metrics.ps1
@@ -6,7 +6,7 @@ $secret = Get-SECSecretValue `
     -Select SecretString | ConvertFrom-Json
 
 $sql_username = $secret.UID
-$sql_password = $secret.PWD
+$sql_password = $secret.PWD #| ConvertTo-SecureString -AsPlainText -Force
 
 $odbc = Get-OdbcDsn -Name "MYSQL" -Platform "32-bit" | Select-Object -ExpandProperty Attribute
 
@@ -14,6 +14,7 @@ $mysqlExe = "C:\Program Files\mysql\MySQL Workbench 8.0 CE\mysql.exe"
 $defaultsFile = Join-Path $env:TEMP "mysql-dbreg-$PID.ini"
 
 try {
+    #create mysql configuration file for database connection
     $content = @"
 [client]
 database=$($odbc.database)
@@ -24,12 +25,14 @@ host=$($odbc.server)
 
     Set-Content -Path $defaultsFile -Value $content -Encoding ASCII -NoNewline
 
-    # in lansa estrict access to the current user where possible - montana 
+    # in lansa estrict access to the current user where possible - montana
     & icacls $defaultsFile /inheritance:r /grant:r "$env:USERNAME:(R,W)" | Out-Default | Write-Host
 
+    #execute the performance statistics for both bind and literal
     Get-Content mysql-performance-metrics-literal.sql | & $mysqlExe --defaults-extra-file=$defaultsFile --skip-column-names
     Get-Content mysql-performance-metrics-bind.sql | & $mysqlExe --defaults-extra-file=$defaultsFile --skip-column-names
 }
 finally {
+    #remove the configuration file
     Remove-Item $defaultsFile -Force -ErrorAction SilentlyContinue
 }
